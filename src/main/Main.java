07963e3a1a12d88be1258e0d4a80fe9be63bef59
simonyarmo <simonyarmo@gmail.com>
2024-04-12 19:58:49 -0500

    Test

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,8 @@ import javax.swing.JFrame;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        ///balls
+        System.out.println("new Game");
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
